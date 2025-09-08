9c1531f69dc2825b89186426351b788c131d191b
M. Namakshenas <m.n.shenas@gmail.com>
2025-09-08 21:15:21 +0330

    [CONFIG] set x86-64 arch

--- configs/config_x86-64.py
+++ configs/config_x86-64.py
@@ -4,7 +4,7 @@ llm = Llama(
     # model_path="models/Phi-4-reasoning-plus-Q4_K_M.gguf",
     # model_path="models/gemma-3-270m-it-Q4_K_M.gguf",
     model_path="models/Qwen3-4B-Thinking-2507-Q4_K_M.gguf",
-    n_gpu_layers=-1, # -1 for Use all GPU (Metal acceleration)
+    n_gpu_layers=5,
     n_ctx=1024*32,
     n_batch=512,
     verbose=False,
@@ -13,5 +13,5 @@ llm = Llama(
     logits_all=False,     # Don't need all logits for chat
     f16_kv=True,
     n_threads=12,
-    # chat_format="chatml",  # or try "phi-3", "openchat", or "auto"
+    # chat_format="chatml", # or try "phi-3", "openchat", or "auto" 
 )
