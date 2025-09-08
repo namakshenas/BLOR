c0ba9471998c974be1ced7cc49ad823e9d0e80f0
M. Namakshenas <m.n.shenas@gmail.com>
2025-09-08 21:52:08 +0330

    [UPD] restrict the sol space bench4

--- benchmarks/b04/user.py
+++ benchmarks/b04/user.py
@@ -1,3 +1,8 @@
 user_query = """
 What is the exact time complexity of single machine scheduling problem minimizing the total earliness and tardiness with distinct processing times and distinct due dates?
+
+One of these answers is correct:
+1. The problem is NP-hard in the ordinary sense.
+2. The problem is NP-hard in the strong sense.
+3. The problem is solvable in polynomial time.
 """
